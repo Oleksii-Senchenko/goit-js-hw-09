cd1b8af2ea5f95ca579a16915c1a7bbb4862ca42
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-06-06 20:57:49 +0000

    Deploying to gh-pages from @ Oleksii-Senchenko/goit-js-hw-09@052ddca00502fc95291a34232dfb9cb343d875d4 🚀

---
+++ 01-color-switcher.0b7e7b20.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),r=document.querySelector("button[data-stop]");e.addEventListener("click",(function({target:a}){pressStart=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.setAttribute("disabled",""),r.removeAttribute("disabled","")})),r.addEventListener("click",(function(){r.setAttribute("disabled",""),e.removeAttribute("disabled",""),clearInterval(pressStart)}));
+//# sourceMappingURL=01-color-switcher.0b7e7b20.js.map
